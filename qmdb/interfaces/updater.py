19a9b763fe81f5da7dc37ed80550d9630c36b11e
Tijl Kindt <tijl.kindt@gmail.com>
2018-03-23 17:13:20 +0100

    Put more emphasis on year for when to update

--- qmdb/interfaces/updater.py
+++ qmdb/interfaces/updater.py
@@ -140,7 +140,7 @@ class Updater(object):
         return np.exp(stats['a_parameter']*np.power(feature, stats['b_parameter']))
 
     @staticmethod
-    def calculate_update_period(year_period_score, crit_pop_period_score, year_power=1,
+    def calculate_update_period(year_period_score, crit_pop_period_score, year_power=2,
                                 crit_pop_power=1):
         period = np.exp((year_power * np.log(year_period_score) +
                          crit_pop_power * np.log(crit_pop_period_score)) /
